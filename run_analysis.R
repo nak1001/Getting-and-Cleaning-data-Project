414aa391231700131c43895443281d451f33ea87
nak1001 <77851638+nak1001@users.noreply.github.com>
2021-03-17 12:33:19 -0400

    Update run_analysis.R
    Added code to allow for the instant use of data.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,30 @@
+##Use this to load in the required packages to run code below
+library(dplyr)
+
+##Next is to download the dataset that this code runs
+filename <- "Coursera_DS3_Final.zip"
+
+## This sees if the archieve already exists
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename, method="curl")
+}  
+
+## This checks to see if the folder exists
+if (!file.exists("UCI_Dataset")) { 
+  unzip(filename) 
+}
+
+##The code below is to assign all the different data frames
+features <- read.table("UCI_Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI_Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI_Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI_Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI_Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI_Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI_Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI_Dataset/train/y_train.txt", col.names = "code")
+
 ##Combines the training and test sets to make one data set.
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
@@ -30,3 +57,9 @@ FinalData <- TidyData %>%
         group_by(subject, activity) %>%
         summarise_all(funs(mean))
 write.table(FinalData, "FinalData.txt", row.name=FALSE)
+
+##The variable names are checked
+str(FinalData)
+
+##Then pull up the "compressed"/"trimmed" data
+FinalData
